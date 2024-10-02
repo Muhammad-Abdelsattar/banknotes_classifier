0caf3753c3c9fbc5a5f964399889705e4c4125ba
Muhammad-Abdelsattar <91436845+Muhammad-Abdelsattar@users.noreply.github.com>
2024-10-02 16:45:35 +0300

    Delete banknotes_classifier/training/utils.py

--- banknotes_classifier/training/utils.py
+++
@@ -1,5 +0,0 @@
-from lightning.pytorch.callbacks import ModelCheckpoint, ModelSummary
-
-callbacks = [ModelCheckpoint(dirpath="./checkpoints", filename="model.ckpt", monitor="accuracy", mode="max"),
-             ModelSummary(),
-             ]
